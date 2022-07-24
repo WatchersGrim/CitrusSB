45b2d6d58b3dae85cc450f752bf5bfa88c7a78ae
WatchersGrim <34408960+WatchersGrim@users.noreply.github.com>
2022-07-23 21:01:54 -0400

    Delete citrussbpref directory

--- citrussbpref/CSBPrefRootListController.h
+++
@@ -1,8 +0,0 @@
-#import <Preferences/PSListController.h>
-#import <CepheiPrefs/HBRootListController.h>
-#import <Cephei/HBPreferences.h>
-#import <Cephei/HBRespringController.h>
-
-@interface CSBPrefRootListController : PSListController
-
-@end
